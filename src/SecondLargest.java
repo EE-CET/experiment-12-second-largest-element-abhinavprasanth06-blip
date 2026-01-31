654d2c76ed82aba83a2c71207477c683bab5a20f
abhinavprasanth06-blip <abhinavprasanth06@gmail.com>
2026-01-31 17:59:54 +0530

    newSecondLargest.java

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,44 +1,25 @@
 import java.util.Scanner;
+
 public class SecondLargest {
-   public static void main(String[] args){
-	System.out.print("enter the size");
-	Scanner sc = new Scanner(System.in);
-	int size = sc.nextInt();
-	int[]arr= new int[size];
-	int max =0;
-	int temp =0;
-	int flag =0;
-	
-	for(int i =0;i<size;i++){
-            System.out.print("enter the elmt");
-            arr[i] = sc.nextInt();
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int max = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if (x > max) {
+                second = max;
+                max = x;
+            } else if (x > second && x < max) {
+                second = x;
+            }
         }
-        
-	max = arr[0];
-	for(int i =0;i<size;i++){
-            if(arr[i]>max){
-		//System.out.print("???");
-		//System.out.println(max);
-		//System.out.println(arr[i]);
-        max = arr[i];
-	//	
-		temp = i; 
-	     }
-		 	
-	}
-	max = arr[0];
-	for(int i =0;i<size;i++){
-        if(i == temp){
-            //System.out.println(temp);
-			continue;	
-	    }
-		else if(arr[i]>max) {
-	//	System.out.println(max);
-		//System.out.println(arr[i]);
-        max = arr[i];
-		
-		}
-	}
-	System.out.println(max);
-   }
+
+        System.out.println(second);
+    }
 }
