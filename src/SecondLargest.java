3c0d5322775eb3f298b8e19195490436d5b54d9a
abhinavprasanth06-blip <abhinavprasanth06@gmail.com>
2026-01-31 17:54:41 +0530

    Update SecondLargest.java

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -29,7 +29,7 @@ public class SecondLargest {
 	max = arr[0];
 	for(int i =0;i<size;i++){
         if(i == temp){
-            System.out.println(temp);
+            //System.out.println(temp);
 			continue;	
 	    }
 		else if(arr[i]>max) {
